9e5350fdac86ea1abcb36cbe995dcfebd979653d
arifatun09 <arifatunnisa.090501@gmail.com>
2021-09-06 13:00:35 +0700

    mengubah halaman welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    Arifatun Nisa <br> Kelas: MI-2E <br> NIM: 2031710118
                                 </div>
                             </div>
                         </div>
